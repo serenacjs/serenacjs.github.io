e866401803053e12731bac4f4cb41cfa32fc9673
Serena C <serena.js.chang@gmail.com>
2026-05-16 22:39:16 -0700

    fix config for deployment

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,7 @@ const nextConfig: NextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true,
-  },
-  basePath: process.env.NODE_ENV === 'production' ? '/personal_web' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/personal_web' : '',
+  }
 }
 
 export default nextConfig
