a665f9ec377137f673cc41418a73160279a660df
Serena C <serena.js.chang@gmail.com>
2026-05-16 23:44:06 -0700

    Updates

--- _next/static/RSZJZI-MbpEFJPPqgZuvT/_buildManifest.js
+++ _next/static/RSZJZI-MbpEFJPPqgZuvT/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/serenacjs.github.io//_next/:path+",
+        "destination": "/serenacjs.github.io/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
